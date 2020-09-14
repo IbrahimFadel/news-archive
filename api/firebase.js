295205659fb7e5cc4d4a363cbbff0eae57b9afbe
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-09-14 13:40:27 -0400

    firebase no admin

--- api/firebase.js
+++ api/firebase.js
@@ -1,13 +1,18 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAcountKey.json');
+const firebase = require('firebase');
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+firebase.initializeApp({
+	apiKey: 'AIzaSyAxC4g7VqDf9IpM96v0eu7IeUfCAXKJubE',
+	authDomain: 'news-archive-81af9.firebaseapp.com',
 	databaseURL: 'https://news-archive-81af9.firebaseio.com',
+	projectId: 'news-archive-81af9',
+	storageBucket: 'news-archive-81af9.appspot.com',
+	messagingSenderId: '309519019266',
+	appId: '1:309519019266:web:1ebeb4442eef374aca261b',
+	measurementId: 'G-SN9BMLXX0G',
 });
 
-const db = admin.database();
+firebase.analytics();
 
 module.exports = {
-	db,
+	db: firebase.database(),
 };
